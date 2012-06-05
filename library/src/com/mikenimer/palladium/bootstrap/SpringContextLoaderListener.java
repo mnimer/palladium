d07a687dee44e9d2d6cd82f2eae6faded453d0ac
Mike Nimer <mikenimer@yahoo.com>
2012-06-05 12:51:12 -0500

    add new ColdFusion Bootstrap Spring Classloader, so you can start up spring in the ColdFusion ClassLoader

---
+++ library/src/com/mikenimer/palladium/bootstrap/SpringContextLoaderListener.java
@@ -0,0 +1,58 @@
+package coldfusion.bootstrap;
+
+import org.springframework.web.context.ContextLoaderListener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+
+/**
+ * User: mnimer
+ * Date: 6/5/12
+ */
+public class SpringContextLoaderListener implements ServletContextListener
+{
+    private ClassLoader bscl;
+    private ServletContextListener listener;
+
+
+    public void contextInitialized(ServletContextEvent servletContextEvent)
+    {
+        try
+        {
+            String listenerClass = "org.springframework.web.context.ContextLoaderListener";
+            ClassloaderHelper cHelper = ClassloaderHelper.getInstance();
+            cHelper.init(servletContextEvent.getServletContext());
+            bscl = cHelper.getClassLoader();
+            //todo: get Adobe to add an initListener to the ColdFusion ClassLoaderHelper
+            listener = (ServletContextListener) cHelper.getClassLoader().loadClass(listenerClass).newInstance();
+        }
+        catch (Exception ex)
+        {
+            throw new RuntimeException(ex);
+        }
+
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        ClassLoader bootstrap = BootstrapClassLoader.instance();
+        Thread.currentThread().setContextClassLoader(bootstrap);
+
+        listener.contextInitialized(servletContextEvent);
+
+        Thread.currentThread().setContextClassLoader(cl);
+
+    }
+
+
+    public void contextDestroyed(ServletContextEvent event)
+    {
+
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(bscl);
+
+        listener.contextDestroyed(event);
+
+        Thread.currentThread().setContextClassLoader(cl);
+
+
+    }
+}
